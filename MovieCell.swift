a220cc2ec7705018b0e3c69f9aa0b5bde95485e3
Diego Ernesto Canon Soto <diegocanon12@gmail.com>
2023-06-29 12:32:18 -0500

    Consumo de paquetes SDWebImage Creacion del metodo setImageUrl

--- MovieCell.swift
+++ MovieCell.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 import UIKit
+import SDWebImage
+import SDWebImageMapKit
 
 class MovieCell : UICollectionViewCell{
     
     
     @IBOutlet weak var movieImage: UIImageView!
+    
+    func setImageUrl(_ url: String){
+        let urlImage = "https://image.tmdb.org/t/p/w400\(url)"
+        movieImage.sd_setImage(with: URL(string: "\(urlImage)"))
+    }
 }
